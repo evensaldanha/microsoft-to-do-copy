672663dfe3a337f2fbd3c0cfe6ea4c1f19f58e83
Even Saldanha <evensaldanhass@gmail.com>
2022-08-11 14:58:12 -0300

    criação do cabeçalho

---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,13 @@
+import Cadastro from './Cadastro';
+
+export default function Header() {
+  return (
+  
+    <div className="cabecalho">
+      <header>
+        <Cadastro></Cadastro>
+      </header>
+    </div>
+    
+  )
+}
