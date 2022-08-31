c9535aa9cc159d0e561d00b3990f6953e829e2d3
Even Saldanha <evensaldanhass@gmail.com>
2022-08-31 19:42:33 -0300

    rotas das paginas

--- src/Rotas/RotasDasPaginas.jsx
+++ src/Rotas/RotasDasPaginas.jsx
@@ -1,12 +1,23 @@
 import { Routes, Route  } from "react-router-dom";
+import Home from "../Componentes/Home";
 import Prazo from "../Componentes/Prazo";
 import Tarefas from "../Componentes/Tarefas";
+import Layout from "../Layout/Layout";
+import Meudia from "../Componentes/MeuDia/meuDia";
+import Importante from "../Componentes/Importante";
+import AtribuidoAMim from "../Componentes/AtribuidoAMim";
 
 export default function RotasDasPaginas() {
   return (
     <Routes>
-        <Route path="/prazos" element={<Prazo/>}></Route>
-        <Route path='/tarefas' element={<Tarefas/>}> </Route>
+      <Route element={<Layout/>}>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/prazos" element={<Prazo/>}/>
+        <Route path='/meudia' element={<Meudia/>} />
+        <Route path='/importante' element={<Importante/>} />
+        <Route path='/atribuidoamim' element={<AtribuidoAMim/>}/>
+        <Route path='/tarefas' element={<Tarefas/>} />
+      </Route>
     </Routes>
   )
 }
