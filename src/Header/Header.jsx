3bc1e92c56094405ded25cee4f78616812e2a094
Even Saldanha <evensaldanhass@gmail.com>
2022-08-31 19:40:19 -0300

    comp. do cadastro

---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import Cadastro from '../DetalheDoCadastro/Cadastro';
+import './header.css';
+
+export default function Header() {
+  return (
+  
+    <div className="cabecalho">
+      <header >
+        <Cadastro></Cadastro>
+      </header>
+    </div>
+    
+  )
+}
