1da8c05adc42175b87de4ce180eb167a650f1757
Even Saldanha <evensaldanhass@gmail.com>
2022-08-31 19:39:40 -0300

    detalhe do cadastro

---
+++ src/DetalheDoCadastro/DetalheDoCadastro.jsx
@@ -0,0 +1,22 @@
+
+export default function DetalheDoCadastro(props) {
+ 
+
+  return (
+    
+    <div className="detalhe-do-cadastro">
+      <label>Nome:
+        <input type='texto'></input>
+      </label>
+      <div> <label>Formação:
+        <input type='texto'></input>
+      </label></div>
+      <div> <label>Período:
+        <input type='number'></input>
+      </label></div>
+      <div> <label> Email:
+        <input type='email'></input>
+      </label></div>
+    </div>
+  )
+}
