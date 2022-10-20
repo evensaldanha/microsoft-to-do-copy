d77b923718334a7574c3a12f3c955e1602a4f49f
Even Saldanha <evensaldanhass@gmail.com>
2022-10-20 17:42:08 -0300

    Criação do comp.Home

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,11 +1,13 @@
-import { useEffect } from "react"
+import '../Home/styleDoHome.css';
 
-export default function Home(){
-    useEffect(() => {
-        console.log("inicializou........................")
-    }, [])
+export default function Home() {
 
-    return(
-        <div>Home</div>
+    return (
+        <div className='imagem-pai'>
+            <h1>Bem vindo(a) ao To-Do</h1>
+            <div className="imagens">
+                <img src="https://cdn.pixabay.com/photo/2012/04/25/00/23/checklist-41335_960_720.png" alt=''/>
+            </div>
+        </div>
     )
 }
