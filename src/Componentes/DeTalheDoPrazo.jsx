f7afe9c60b9900093e288935bac59a0bf7c674ff
Even Saldanha <evensaldanhass@gmail.com>
2022-10-29 15:42:29 -0300

    formatação do cod.

--- src/Componentes/DeTalheDoPrazo.jsx
+++ src/Componentes/DeTalheDoPrazo.jsx
@@ -1,25 +1,18 @@
 import Calendar from 'react-calendar';
 
 export default function DetalheDoPrazo() {
-{/*const [day, setDay] = useState(0)*/}
     return (
         <div className="detalhe-do-prazo">
             <label>Semanal
                 <input type="checkbox" name='parazos-checbox' value='semanal' />
             </label>
-
             <p>
-                <label>Mensal </label>
+                <label>Mensal
+                    <input type="checkbox" name="prazos-mensais" value="mensal" />
+                </label>
             </p>
-            {/*<div>
-                <div>{day}</div>
-
-                <button onClick={() => setDay((oldDay) => oldDay + 1)}>+</button>
-
-                <button onClick={() => setDay((oldDay) => oldDay - 1)}>-</button>
-    </div>*/} 
             <div>
-            <Calendar></Calendar>
+                <Calendar></Calendar>
             </div>
         </div>
     )
