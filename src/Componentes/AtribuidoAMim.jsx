5bf369b451a145c17865097371048449b6d6ed60
Even Saldanha <evensaldanhass@gmail.com>
2022-12-15 17:29:55 -0300

    criado o componente

---
+++ src/Componentes/AtribuidoAMim.jsx
@@ -0,0 +1,21 @@
+
+
+
+export default function AtribuidoAMim() {
+    return (
+      
+        <div>olá</div>
+    )
+}
+
+
+
+
+// export default function ParOuImpar(props) {
+
+//   return (
+//     <div>
+//       Seja bem vindo  <strong>{props.titulo}{props.nome ? "verdadeiro" : "falso"}</strong>        
+//     </div>
+//   )
+// }
