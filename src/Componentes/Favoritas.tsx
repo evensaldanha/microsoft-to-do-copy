a1cd0a2bb19cf001b8551ebc53f7fec46ecf1d1d
Even Saldanha <evensaldanhass@gmail.com>
2022-12-15 17:33:21 -0300

    elaborado a logica para funcionar

---
+++ src/Componentes/Favoritas.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { TransactionContext } from "../TransactionContext";
+import "../Style/favoritas.css";
+
+export default function Favoritas() {
+  const { tarefas, setTarefas } = useContext(TransactionContext);
+
+  return (
+    <div className="div-favoritas">
+      {tarefas
+        .filter((tarefa) => {
+          return tarefa.isFavorite;
+        })
+        .map(( tarefa, index ) => (
+          <div className="div-do-map" key={index}>{tarefa.name}</div>
+        ))}
+    </div>
+  );
+}
