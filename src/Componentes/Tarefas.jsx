f4b297b31fb392392fe41092d88a2b9d413053d9
Even Saldanha <evensaldanhass@gmail.com>
2023-01-04 17:00:52 -0300

    uso do map para receber as tarefas do meu dia

--- src/Componentes/Tarefas.jsx
+++ src/Componentes/Tarefas.jsx
@@ -1,14 +1,22 @@
-import '../Style/stiloDeTarefas.css';
-import Card from '@mui/material/Card';
-import { CardActionArea, CardHeader } from '@mui/material';
-import CardContent from '@mui/material/CardContent';
-
+import "../Style/stiloDeTarefas.css";
+// import Card from '@mui/material/Card';
+// import { CardActionArea, CardHeader } from '@mui/material';
+// import CardContent from '@mui/material/CardContent';
+import { TransactionContext } from "../TransactionContext";
+import { useContext } from "react";
 
 
 export default function Tarefas() {
+
+  const { tarefas, setTarefas } = useContext(TransactionContext);
+
   return (
     <div className="flex-container">
-      <Card className='card'>
+
+      {tarefas.map(( tarefa, index ) => (
+      <div className='div-tarefas' key={index}>{tarefa.name}{tarefa.description}</div>
+        ))}
+      {/* <Card className='card'>
         <CardActionArea>
           <CardHeader
             title='A fazer'>
@@ -24,26 +32,21 @@ export default function Tarefas() {
           <CardHeader
             title='Em andamento'> </CardHeader>
           <CardContent>
-           <div>Tarefa 1</div>
-          <div>Tarefa 2</div>
+            <div>Tarefa 1</div>
+            <div>Tarefa 2</div>
           </CardContent>
 
         </CardActionArea>
       </Card>
       <Card className='card'>
-          <CardHeader
-            title='Concluidas'> </CardHeader>
-          <CardContent>
+        <CardHeader
+          title='Concluidas'> </CardHeader>
+        <CardContent>
           <div>Tarefa 1</div>
           <div>Tarefa 2</div>
-          </CardContent>
-      </Card>
-      <Card>
-      <div className='div-tarefas'>
-        oi
-      </div>
-     </Card>
+        </CardContent>
+      </Card> */}
     </div>
-    
-  ) 
+
+  )
 }
