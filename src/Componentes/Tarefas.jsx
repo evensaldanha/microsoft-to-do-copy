3d87a1f6bef5758892df9f860dd6195543b9c20f
Even Saldanha <evensaldanhass@gmail.com>
2023-01-05 13:55:12 -0300

    Utilizando a tag accordion do MUI

--- src/Componentes/Tarefas.jsx
+++ src/Componentes/Tarefas.jsx
@@ -3,49 +3,68 @@ import "../Style/stiloDeTarefas.css";
 // import { CardActionArea, CardHeader } from '@mui/material';
 // import CardContent from '@mui/material/CardContent';
 import { TransactionContext } from "../TransactionContext";
-import { useContext } from "react";
-
+import { useContext, useState } from "react";
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function Tarefas() {
 
   const { tarefas, setTarefas } = useContext(TransactionContext);
 
+ 
+
   return (
     <div className="flex-container">
-
-      {tarefas.map(( tarefa, index ) => (
-      <div className='div-tarefas' key={index}>{tarefa.name}{tarefa.description}</div>
+        <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>A fazer</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Olá
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Concluida</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Olá
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Em andamento</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Olá
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <div>
+        {tarefas.map((tarefa, index) => (
+          <div className="div-tarefas" key={index}>{tarefa.name}{tarefa.description}</div>
         ))}
-      {/* <Card className='card'>
-        <CardActionArea>
-          <CardHeader
-            title='A fazer'>
-          </CardHeader>
-          <CardContent>
-            <div>Tarefa 1</div>
-            <div>Tarefa 2</div>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className='card '>
-        <CardActionArea>
-          <CardHeader
-            title='Em andamento'> </CardHeader>
-          <CardContent>
-            <div>Tarefa 1</div>
-            <div>Tarefa 2</div>
-          </CardContent>
-
-        </CardActionArea>
-      </Card>
-      <Card className='card'>
-        <CardHeader
-          title='Concluidas'> </CardHeader>
-        <CardContent>
-          <div>Tarefa 1</div>
-          <div>Tarefa 2</div>
-        </CardContent>
-      </Card> */}
+      </div>
     </div>
 
   )
