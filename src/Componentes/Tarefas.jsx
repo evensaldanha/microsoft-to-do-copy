de790060315b1a28fa3643042d161a1f8ee724d7
Even Saldanha <evensaldanhass@gmail.com>
2023-01-27 15:53:28 -0300

    utilizei o map

--- src/Componentes/Tarefas.jsx
+++ src/Componentes/Tarefas.jsx
@@ -1,79 +1,30 @@
 import "../Style/stiloDeTarefas.css";
-// import Card from '@mui/material/Card';
-// import { CardActionArea, CardHeader } from '@mui/material';
-// import CardContent from '@mui/material/CardContent';
 import { TransactionContext } from "../TransactionContext";
-import { useContext, useState } from "react";
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useContext} from "react";
+
+
+
 
 export default function Tarefas() {
 
   const { tarefas, setTarefas } = useContext(TransactionContext);
 
- 
 
-  return (
+    return (
 
-    <div className="flex-container">
-        <Accordion className="card">
-        <AccordionSummary className="div-tarefas"
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>A fazer</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Olá
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion className="card">
-        <AccordionSummary className="div-tarefas"
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Concluida</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Olá
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion className="card">
-        <AccordionSummary className="div-tarefas"
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Em andamento</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Olá
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <div className="div-tarefa">
-        {tarefas.map((tarefa, index) => (
-          <div className="div-tarefas" key={index}>{tarefa.name}{tarefa.description}</div>
-          )).filter(({ tarefa }) => {
-            if (tarefa.aFazer === true) {
-              return false;
-            } else {
-              return true;
-            }
-          })
-        }
+      <div className="flex-container">
+        <div className="div-tarefa">
+          {tarefas.map((tarefa, index) => (
+            <div
+              className="div-tarefas"
+              key={index}
+            >
+              {tarefa.name}{tarefa.description}
+              
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
 
-  )
+    )
 }
