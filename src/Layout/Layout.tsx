704d5593659ffff5a3c7591a1b361f167506824d
Even Saldanha <evensaldanhass@gmail.com>
2023-01-30 14:12:53 -0300

    exclusão do comp header

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,15 +1,14 @@
 import { LayoutRouteProps, Outlet } from "react-router-dom";
-import Header from "../Header/Header";
+// import Header from "../Header/Header";
 import ListaDoMenu from "../ListaDoMenu/ListaDoMenu";
 import './layoutStilo.css';
 
 export default function Layout(props: LayoutRouteProps) {
   return (
     <div className="header-layout">
-       <div > <Header></Header> </div>
+       {/* <div > <Header></Header> </div> */}
        <div className="lista-do-menu-no-layout">
        <div > <ListaDoMenu></ListaDoMenu> </div>
-
         <Outlet></Outlet>
         </div>
     </div>
