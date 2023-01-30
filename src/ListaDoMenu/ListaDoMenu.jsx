d31eb96683ae4369da8682437f103a0c430a194d
Even Saldanha <evensaldanhass@gmail.com>
2023-01-30 14:14:46 -0300

    reorganizei os itens

--- src/ListaDoMenu/ListaDoMenu.jsx
+++ src/ListaDoMenu/ListaDoMenu.jsx
@@ -5,7 +5,6 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import HomeIcon from '@mui/icons-material/Home';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import GradeIcon from '@mui/icons-material/Grade';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import { useState } from "react";
 import { Button } from "@mui/material";
 
@@ -13,30 +12,30 @@ import { Button } from "@mui/material";
 
 export default function ListaDoMenu() {
   const [visivel, setVisivel] = useState("")
-  
+
 
   return (
     <div className="lista-do-menu">
-      <Button variant="contained" onClick= {(e) => setVisivel(visivelAtual => !visivelAtual) }>Menu</Button>
-      
+      <Button variant="contained" onClick={(e) => setVisivel(visivelAtual => !visivelAtual)}>Menu</Button>
+
       <div className={visivel ? "" : "invisible"}>
-       <div className="lista-dos-itens">
-        <HomeIcon></HomeIcon>
-        <Link to="/home">Home</Link>
-      
-        <AccessAlarmIcon > </AccessAlarmIcon>
-        <Link to="/prazos">Prazo</Link>
-      
-        <AddTaskIcon></AddTaskIcon>
-        <Link to="/tarefas">Tarefas</Link>
-      
-        <AcUnitIcon></AcUnitIcon>
-        <Link to="/meuDia">Meu Dia</Link>
-      
-        <GradeIcon></GradeIcon>
-        <Link to="/favoritas">Favoritas</Link>
-      
-      </div>
+        <div className="lista-dos-itens">
+          <HomeIcon></HomeIcon>
+          <Link to="/home">Home</Link>
+
+          <AcUnitIcon></AcUnitIcon>
+          <Link to="/meuDia">Meu Dia</Link>
+
+          <GradeIcon></GradeIcon>
+          <Link to="/favoritas">Favoritas</Link>
+
+          <AddTaskIcon></AddTaskIcon>
+          <Link to="/tarefas">Tarefas</Link>
+
+          <AccessAlarmIcon > </AccessAlarmIcon>
+          <Link to="/prazos">Prazo</Link>
+
+        </div>
       </div>
     </div>
   )
