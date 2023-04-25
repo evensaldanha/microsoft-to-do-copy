6725acd8391ca047683bfcb74ecaca6bd270207f
Even Saldanha <evensaldanhass@gmail.com>
2023-04-25 14:24:33 -0300

    nao estou utilizando

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,13 +1,14 @@
-import '../Home/styleDoHome.css';
+// import '../Home/styleDoHome.css';
 
-export default function Home() {
+// export default function Home() {
 
-    return (
-        <div className='imagem-pai'>
-            <h1>Bem vindo(a) ao To-Do</h1>
-            <div className="imagens">
-                <img src="https://cdn.pixabay.com/photo/2012/04/25/00/23/checklist-41335_960_720.png" alt=''/>
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div className='imagem-pai'>
+//             <h1>Bem vindo(a) ao To-Do</h1>
+//             <div className="imagens">
+//                 <img src="https://cdn.pixabay.com/photo/2012/04/25/00/23/checklist-41335_960_720.png" alt=''/>
+//             </div>
+           
+//         </div>
+//     )
+// }
