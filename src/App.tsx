04db872b3c026e2d35e9031b9f893cb4c2550a61
Even Saldanha <evensaldanhass@gmail.com>
2023-04-25 14:11:07 -0300

    coloquei o import de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import RotasDasPaginas from "./Rotas/RotasDasPaginas";
+import  "../src/App.css";
 
 function App() {
   return (
     <div className="App">
        <RotasDasPaginas></RotasDasPaginas>
-       
     </div>
   );
 }
