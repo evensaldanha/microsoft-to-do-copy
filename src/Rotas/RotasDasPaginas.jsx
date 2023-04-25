72b78f376dd981de77d1ac055537aa31039abd33
Even Saldanha <evensaldanhass@gmail.com>
2023-04-25 14:58:43 -0300

    utilizacao de routes para as  rotas

--- src/Rotas/RotasDasPaginas.jsx
+++ src/Rotas/RotasDasPaginas.jsx
@@ -1,21 +1,22 @@
 import { Routes, Route  } from "react-router-dom";
 import Home from "../Componentes/Home";
-import Prazo from "../Componentes/Prazo";
+import Planejado from "../Componentes/Planejado";
 import Tarefas from "../Componentes/Tarefas";
 import Layout from "../Layout/Layout";
 import Meudia from "../Componentes/MeuDia/meuDia";
 import Favoritas from "../Componentes/Favoritas";
+import Testanto from "../Componentes/teste/Testando";
 
 export default function RotasDasPaginas() {
   return (
     <Routes>
       <Route element={<Layout/>}>
-        <Route path="/" element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Meudia/>} />
         <Route path='/meudia' element={<Meudia/>} />
         <Route path='/favoritas' element={<Favoritas/>} />
-        <Route path="/prazos" element={<Prazo/>}/>
+        <Route path="/planejado" element={<Planejado/>}/>
         <Route path='/tarefas' element={<Tarefas/>} />
+        <Route path="/testando" element={<Testanto/>}/>
       </Route>
     </Routes>
   )
