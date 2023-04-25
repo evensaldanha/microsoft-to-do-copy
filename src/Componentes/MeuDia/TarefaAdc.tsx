ff8f26c0e65c84cf97ffda825ab8a9619d6aba2a
Even Saldanha <evensaldanhass@gmail.com>
2023-04-25 11:03:13 -0300

    implementado a func. de cortar a tarefa quando conc. so cm css

--- src/Componentes/MeuDia/TarefaAdc.tsx
+++ src/Componentes/MeuDia/TarefaAdc.tsx
@@ -2,39 +2,55 @@ import { Paper, Checkbox, CheckboxProps } from "@mui/material";
 import "./styledaTarefaAdc.css";
 import Favorite from "@mui/icons-material/Favorite";
 import { Task } from "../../types/task";
-const label = { inputProps: { "aria-label": "Checkbox demo" } };
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useContext, useState } from "react";
+import { TransactionContext } from "../../TransactionContext";
 
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 interface TarefaProps extends Task {
   selectOnChange: CheckboxProps["onChange"];
   isFavoriteOnChange: CheckboxProps["onChange"];
+  index: number;
 }
 
 export default function Tarefa({
-  name,
+  title,
   description,
   completed,
   selectOnChange,
   isFavorite,
   isFavoriteOnChange,
- 
+  index,
 }: TarefaProps) {
+  const { tasks, setTasks } = useContext(TransactionContext);
+
+  function removingTask() {
+    const removingItem = [...tasks];
+    removingItem.splice(index, 1);
+    if (setTasks) {
+      setTasks(removingItem);
+    }
+  }
   return (
-    <div className="container">
-      <Paper className="paper">
+    <div className={completed ? "line" : ""}>
+                
+      <Paper >
         <>
-          <Checkbox
-          onChange={selectOnChange} checked={completed}
-        />
-          {name}
+          <Checkbox onChange={selectOnChange} checked={completed} />
+          {title}
           {description}
           <Checkbox
             {...label}
             checked={isFavorite}
             checkedIcon={<Favorite />}
-            onChange= {isFavoriteOnChange}
+            onChange={isFavoriteOnChange}
           />
+          <Button onClick={removingTask}>
+            <DeleteIcon />
+          </Button>
         </>
       </Paper>
     </div>
-  );
+  )
 }
