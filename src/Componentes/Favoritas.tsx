7ad86dbe21de12fbf7b8535a4788e7556e5da5a7
Even Saldanha <evensaldanhass@gmail.com>
2023-04-25 14:16:50 -0300

    Foi utilizado filter e map

--- src/Componentes/Favoritas.tsx
+++ src/Componentes/Favoritas.tsx
@@ -1,19 +1,29 @@
-import { useContext } from "react";
+import { useContext as useContext } from "react";
 import { TransactionContext } from "../TransactionContext";
 import "../Style/favoritas.css";
+import GradeIcon from '@mui/icons-material/Grade';
+import Paper from "@mui/material/Paper";
 
-export default function Favoritas() {
-  const { tarefas, setTarefas } = useContext(TransactionContext);
+export default function Favorite() {
+  const { tasks, setTasks } = useContext(TransactionContext);
 
   return (
-    <div className="div-favoritas">
-      {tarefas
-        .filter((tarefa) => {
-          return tarefa.isFavorite;
-        })
-        .map(( tarefa, index ) => (
-          <div className="div-do-map" key={index}>{tarefa.name} {tarefa.description}</div>
-        ))}
+    <div className="div-favoritas" style={{marginLeft: "24px", marginRight: "24px" }}>
+      <div style={{display:"flex",flexDirection:"row", color:"#2564cf" }}>
+      <GradeIcon ></GradeIcon>
+      <h1 >Importante</h1>
+      </div>
+      {
+        tasks
+          .filter((tarefa) => {
+            return tarefa.isFavorite;
+          })
+          .map(( tarefa, index ) => (
+            <Paper className="div-do-map" key={index}>
+              {tarefa.title} {tarefa.description}
+            </Paper>
+          ))
+      }
     </div>
-  );
+  )
 }
