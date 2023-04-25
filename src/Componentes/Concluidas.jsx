718eb81a0d08fa8bfecf32fcf8ad99cdbe13efd4
Even Saldanha <evensaldanhass@gmail.com>
2023-04-25 14:13:16 -0300

    atualizacao dos nomes e estilo

--- src/Componentes/Concluidas.jsx
+++ src/Componentes/Concluidas.jsx
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react"
 import { TransactionContext } from "../TransactionContext";
 import { useContext } from "react";
-import Tarefa from "./MeuDia/TarefaAdc";
+import  Tarefa  from "./MeuDia/TarefaAdc";
 
-export default function Concluidas() {
-    const { tarefas, setTarefas } = useContext(TransactionContext);
+
+export default function Completed() {
+    const { tasks, setTasks } = useContext(TransactionContext);
     const [completed, setCompleted] = useState([false])
 
     useEffect(() => {
-        setCompleted(() => tarefas.filter((tarefa) => tarefa.completed === true))
-    }, [tarefas])
+        setCompleted(() => tasks.filter((tarefa) => tarefa.completed === true))
+    }, [tasks])
 
     return (
-        <div>
-            {completed.map(tarefa =>
-                    <Tarefa
-                        completed={tarefa.completed}
-                        name={tarefa.name}
-                        description={tarefa.description}
-                    >
-                    </Tarefa>
-                )
-            }
+        <div 
+        style={{marginLeft: "24px", marginRight: "24px"  }}>
+                {completed.map(tarefa =>
+                        <Tarefa 
+                            completed={tarefa.completed}
+                            name={tarefa.title}
+                            description={tarefa.description}
+                        >
+                        </Tarefa>
+                    )
+                }
         </div>
     )
 }
